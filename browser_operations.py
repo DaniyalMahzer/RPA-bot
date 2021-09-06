6379c2b22bad3beea958eb6d57989e9f32bb0f7a
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 11:00:49 +0500

    FIXES TASK

--- browser_operations.py
+++ browser_operations.py
@@ -4,6 +4,9 @@ from RPA.Browser.Selenium import Selenium
 def new_repo():
     browser = Selenium()
     browser.open_available_browser("github.com")
+    browser.wait_until_page_contains_element('/html/body/div[3]/main/div/div[4]/form/div/input[1]')
+    browser.input_text('/html/body/div[3]/main/div/div[4]/form/input[2]', "daniyaltestdjango@gmail.com")
+    browser.input_text('/html/body/div[3]/main/div/div[4]/form/div/input[1]', "testaccount1212")
     browser.wait_until_page_contains_element('/html/body/div[4]/div/aside/div[2]/div[1]/div/h2/a')
     browser.click_button('/html/body/div[4]/div/aside/div[2]/div[1]/div/h2/a')
     browser.input_text('/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input', "new repository by bot")
