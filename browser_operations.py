32e26ebca8cd981ffdcf4d992b3d048e1925514f
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 11:40:26 +0500

    removed stuff

--- browser_operations.py
+++ browser_operations.py
@@ -5,7 +5,6 @@ def new_repo():
     browser = Selenium()
     browser.open_available_browser("https://github.com/")
     browser.find_element('//a[@class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
-    browser.wait_until_page_contains_element('/html/body/div[3]/main/div/div[4]/form/div/input[1]')
     browser.input_text('//form[@class="form-control input-block"]', "daniyaltestdjango@gmail.com")
     browser.input_text('/html/body/div[3]/main/div/div[4]/form/div/input[1]', "testaccount1212")
     browser.open_available_browser("https://github.com/")
