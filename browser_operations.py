caa7606c77ac5df00e5f8fcfb24076a9be7bf712
DaniyalMahzer <85176106+DaniyalMahzer@users.noreply.github.com>
2021-09-06 10:53:18 +0500

    url

--- browser_operations.py
+++ browser_operations.py
@@ -3,7 +3,7 @@ from RPA.Browser.Selenium import Selenium
 
 def new_repo():
     browser = Selenium()
-    browser.open_available_browser("github.com")
+    browser.open_available_browser("https://github.com/")
     browser.wait_until_page_contains_element('/html/body/div[4]/div/aside/div[2]/div[1]/div/h2/a')
     browser.click_button('/html/body/div[4]/div/aside/div[2]/div[1]/div/h2/a')
     browser.input_text('/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input', "new repository by bot")
