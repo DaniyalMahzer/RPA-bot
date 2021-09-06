5bb5ff82265fa7186af3ba0355b05bb886387dc6
DaniyalMahzer <85176106+DaniyalMahzer@users.noreply.github.com>
2021-09-06 11:20:37 +0500

    update

--- browser_operations.py
+++ browser_operations.py
@@ -4,7 +4,7 @@ from RPA.Browser.Selenium import Selenium
 def new_repo():
     browser = Selenium()
     browser.open_available_browser("https://github.com/")
-    browser.click_link('/html/body/div[1]/header/div/div[1]/div[2]/a')
+    browser.find_element('/html/body/div[1]/header/div/div[2]').click()
     browser.wait_until_page_contains_element('/html/body/div[3]/main/div/div[4]/form/div/input[1]')
     browser.input_text('/html/body/div[3]/main/div/div[4]/form/input[2]', "daniyaltestdjango@gmail.com")
     browser.input_text('/html/body/div[3]/main/div/div[4]/form/div/input[1]', "testaccount1212")
